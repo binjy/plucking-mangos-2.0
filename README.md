4addb263ecaf87e9ae30df33b6bc02305ec00067
binjy <72844741+binjy@users.noreply.github.com>
2021-03-23 20:34:53 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# plucking-mangos-2.0
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ Tree.js
@@ -0,0 +1,24 @@
+
+ class Tree {
+    constructor(x,y,width,height) {
+   //   var options = {
+     //     isStatic:false
+     // }
+      this.body = Bodies.rectangle(x,y,width,height);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("tree.png");
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push()
+      translate(pos.x,pos.y)
+      rectMode(CENTER);
+      fill("brown");
+      imageMode(CENTER)
+      image(this.image,0,0,this.width,this.height)
+     // rect(pos.x, pos.y, this.width, this.height);
+     pop()
+    }
+  };
